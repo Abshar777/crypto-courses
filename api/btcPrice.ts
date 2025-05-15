faa59614d4f7c21d69982b26b4d80771bdefdd43
Abshar777 <absharameen625@gmail.com>
2025-05-15 18:07:40 +0530

    feat: market cap and btc price added

--- api/btcPrice.ts
+++ api/btcPrice.ts
@@ -7,7 +7,7 @@ export const getBtcPrice = async () => {
 
 
 export const marketCap = async () => {
-  const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_by_total_volume&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en");
+  const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin");
   console.log(response.data)
   return response.data[0].market_cap;
 };  
